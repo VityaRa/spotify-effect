e3dc9ba2200bd15823f2c838d2a43a2b3877cc85
VityaRa <blackops2in@mail.ru>
2021-09-06 10:26:32 +0300

    feat: add inputs, validation

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const el = document.querySelector('.container')
-el.style.backgroundColor = `linear-gradient(0deg, rgba(20,20,20,1) 0%, rgba(20,20,20,1) 100%) no-repeat`
+el.style.backgroundColor = `rgb(20,20,20) no-repeat`
 el.style.transition = `.3s ease-in`
 
+const btnContainer = document.querySelector('.btn__container')
 const redBtn = document.querySelector('.red')
 redBtn.style.backgroundColor = `#404040`
 const blueBtn = document.querySelector('.blue')
 blueBtn.style.backgroundColor = `#4f4343`
+
+const submitBtn = document.querySelector('.btn__create')
+const colorInput = document.querySelector('.input__color')
+const textInput = document.querySelector('.text')
+
+
+const DEFAULT_COLOR = '#c2c5cc'
+colorInput.value = DEFAULT_COLOR
 
 const changeBackground = (color) => {
     el.style.backgroundColor = `${color}`
@@ -19,13 +28,61 @@ const mouseLeave = () => {
     changeBackground(`rgba(20,20,20,1)`)
 }
 
-
-[redBtn, blueBtn].forEach(item => {
-    item.addEventListener('mouseover', () => {
-        mouseOver(item.style.backgroundColor);
-    })
-    item.addEventListener('mouseleave', () => {
+window.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('btn__choose')) {
+        const btn = e.target;
+        mouseOver(btn.style.backgroundColor)
+    } else {
         mouseLeave()
-    })
+    }
 })
 
+
+// [redBtn, blueBtn].forEach(item => {
+//     item.addEventListener('mouseover', () => {
+//         mouseOver(item.style.backgroundColor);
+//     })
+//     item.addEventListener('mouseleave', () => {
+//         mouseLeave()
+//     })
+// })
+
+const getColor = () => {
+    return colorInput.value;
+}
+
+const getText = () => {
+    return textInput.value
+}
+
+const createButton = (color, text) => {
+    const btn = document.createElement('button')
+    btn.style.backgroundColor = color;
+    btn.innerHTML = text;
+    btn.classList.add('btn__choose')
+
+    return btn
+}
+
+const clearInput = () => {
+    colorInput.value = DEFAULT_COLOR
+    textInput.value = ``
+}
+
+const submitClickHandler = () => {
+    const color = getColor();
+    const text = getText();
+    if (!text.trim()) {
+        alert('Введите текст кнопки!')
+        return
+    }
+    
+    const btn = createButton(color, text)
+    btnContainer.appendChild(btn)
+    clearInput()
+
+}
+
+submitBtn.addEventListener('click', () => {
+    submitClickHandler()
+})
